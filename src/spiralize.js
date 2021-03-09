265db44b7b8147948d521236c6001380f7a348ab
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-03-09 10:25:32 +0300

    Такое себе но работающее решение задачи ~_~

--- src/spiralize.js
+++ src/spiralize.js
@@ -1,11 +1,55 @@
 const spiralize = (size) => {
-    const array = Array.from({ length: size }).map(
+    const matrix = Array.from({ length: size }).map(
         (_) => Array.from({ length: size }).map(
             (_) => 0
         )
     );
 
-    return array;
+    let subMatrixSize = size;
+
+    let i = 0;
+
+    while (subMatrixSize > 0) {
+        let m = i * 2;
+        let n = m;
+
+        while (m < subMatrixSize + (i * 2)) {
+            matrix[n][m] = 1;
+            m++;
+        }
+        m--;
+        n++;
+
+        while (n < subMatrixSize + (i * 2)) {
+            matrix[n][m] = 1;
+            n++;
+        }
+        n--;
+        m--
+        
+        while (m >= 0 + (i * 2)) {
+            matrix[n][m] = 1;
+            m--;
+        }
+        m++;
+        n--;
+
+        while (n > 1 + (i * 2)) {
+            matrix[n][m] = 1;
+            n--;
+        }
+        n++;
+        m++;
+
+        subMatrixSize -= 4;
+        i++;
+
+        if (matrix[n][m] === 0 && subMatrixSize > 0) matrix[n][m] = 1;
+    }
+
+    return matrix;
 };
+
+spiralize(5);
 
 module.exports = spiralize;
